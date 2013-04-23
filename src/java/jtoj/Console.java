987eac6a32ed7fafaf52b15c708d6b362f3a782f
Graham Brooks <graham@grahambrooks.com>
2013-04-23 14:59:59 -0400

    Working generation of an AST

---
+++ src/java/jtoj/Console.java
@@ -0,0 +1,74 @@
+package jtoj;
+
+import jtoj.Java.JavaLexer;
+import jtoj.Java.JavaParser;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.Utils;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.Tree;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Console {
+    public static void main(String[] args) throws IOException {
+
+        JavaLexer lexer = new JavaLexer(new ANTLRFileStream(args[0]));
+        JavaParser parser = new JavaParser(new CommonTokenStream(lexer));
+
+        JavaParser.CompilationUnitContext compilationUnitContext = parser.compilationUnit();
+
+        System.out.print(toStringTree(compilationUnitContext, parser));
+    }
+
+    public static String toStringTree(Tree t, Parser recog) {
+        String[] ruleNames = recog != null ? recog.getRuleNames() : null;
+        List<String> ruleNamesList = ruleNames != null ? Arrays.asList(ruleNames) : null;
+        return toStringTree(t, ruleNamesList);
+    }
+
+    public static String toStringTree(Tree t, List<String> ruleNames) {
+        String s = Utils.escapeWhitespace(getNodeText(t, ruleNames), false);
+        if (t.getChildCount() == 0) return s;
+        StringBuilder buf = new StringBuilder();
+        buf.append("(");
+        s = Utils.escapeWhitespace(getNodeText(t, ruleNames), false);
+        buf.append(s);
+        buf.append(' ');
+        for (int i = 0; i < t.getChildCount(); i++) {
+            if (i > 0) buf.append(' ');
+            buf.append(toStringTree(t.getChild(i), ruleNames));
+        }
+        buf.append(")");
+        return buf.toString();
+    }
+
+    public static String getNodeText(Tree t, List<String> ruleNames) {
+        if (ruleNames != null) {
+            if (t instanceof RuleNode) {
+                int ruleIndex = ((RuleNode) t).getRuleContext().getRuleIndex();
+                return ruleNames.get(ruleIndex);
+            } else if (t instanceof ErrorNode) {
+                return t.toString();
+            } else if (t instanceof TerminalNode) {
+                Token symbol = ((TerminalNode) t).getSymbol();
+                if (symbol != null) {
+                    return String.format("[%s %d %d]", symbol.getText(), symbol.getLine(), symbol.getCharPositionInLine());
+                }
+            }
+        }
+        // no recog for rule names
+        Object payload = t.getPayload();
+        if (payload instanceof Token) {
+            return ((Token) payload).getText();
+        }
+        return t.getPayload().toString();
+    }
+
+}
